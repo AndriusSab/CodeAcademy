821361ca262f2c1dac783b3ff6c70a4efa9e5d2c
Andrius Sabaliauskas <andrius.sabaliauskass@gmail.com>
2023-02-08 19:50:56 +0200

    adding lesson2

---
+++ lesson2/string_operation.py
@@ -0,0 +1,24 @@
+# letter = "A"
+# name = "Code academy"
+# sentence = " I really enjoy learning python"
+# print(name[10])
+# print(name[-1])
+# print(name[0:4]) # paima rezi t.y 4 simbolius is "name"
+# print(name[1:-3])
+# print(name[0:4])
+# print(name[0:4:-1])
+# print (name rplace("o" , "0")) #pakeicia, galima keisti ir visa zodi
+print (name split())
+# print (name.startwith(Z)) # true False komandos jei prasideda 
+# print (name.swapcase())
+#uzkomentuoti galima ctrl + /
+
+greeting = "Hello, my name is"
+name = "Tom"
+
+completed_greeting = f"{greeting} {name}"
+print(completed_greeting)
+
+age = 5 
+sentence = f"I am {age} year(s)" # {} priskiria kintamaji siuo atveju age reiksme "5"
+print (sentence)
