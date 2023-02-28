50a0e73e453be74ac7f9e47f34564339b5debc52
Andrius Sabaliauskas <andrius.sabaliauskass@gmail.com>
2023-02-28 18:07:00 +0200

    adding all

--- lesson16/virtualen.py
+++ lesson16/virtualen.py
@@ -5,10 +5,10 @@ from random_word import RandomWords
 words =[]
 
 r = RandomWords()
-for i in range (5):
+for word in range (5):
     word = r.get_random_word().upper()
     words.append(word)
-
 words.sort()
 print(words)        
-    
+  
+ 
